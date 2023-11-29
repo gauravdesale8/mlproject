790e3ebe787b7d09213dbe2a90d32ae5f94a97fa
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2023-11-30 00:30:40 +0530

    logging and exception

--- template.py
+++ template.py
@@ -10,21 +10,22 @@ list_of_files=[
     # ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/components/data_ingestion.py"
+    f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/components/data_transformation.py",
     f"src/{project_name}/components/model_trainer.py",
-    f"src/{project_name}/component/model_monitering.py"
+    f"src/{project_name}/component/model_monitering.py",
     f"src/{project_name}/pipelines/__init__.py",
     f"src/{project_name}/pipelines/training_pipelines.py",
-    f"src/{project_name}/pipelines/prediction_pipeline.py"
+    f"src/{project_name}/pipelines/prediction_pipeline.py",
     f"src/{project_name}/exception.py",
     f"src/{project_name}/logger.py",
-    f"src/{project_name}/utils.py"
+    f"src/{project_name}/utils.py",
     "app.py",
     "main.py",
     "Dockerfile",
     "requirements.txt",
-    "setup.py"
+    "setup.py",
+    "app.py"
 ]
 
 for filepath in list_of_files:
