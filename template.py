cdffecda00a670bf055e62285bb0d4f63461d9ea
Gaurav  Desale <45401708+gauravdesale8@users.noreply.github.com>
2023-12-11 15:06:25 +0530

    data_transformation

--- template.py
+++ template.py
@@ -4,9 +4,9 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-project_name="mlproject"
+project_name = "mlproject"
 
-list_of_files=[
+list_of_files = [
     # ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
@@ -25,7 +25,7 @@ list_of_files=[
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "app.py"
+    "app.py",
 ]
 
 for filepath in list_of_files:
@@ -33,14 +33,13 @@ for filepath in list_of_files:
     filedir, filename = os.path.split(filepath)
 
     if filedir != "":
-        os.makedirs(filedir,exist_ok=True)
+        os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating Directory:{filedir} for the file {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, 'w') as f:
+        with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
     else:
         logging.info(f"{filename} is already exists")
-
